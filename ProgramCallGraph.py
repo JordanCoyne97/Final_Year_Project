b50ac2335fa6e9bef41f72ed2aed6949163cd3a5
JordanCoyne97 <jordancoyne@hotmail.com>
2020-01-17 16:16:41 +0000

    pushing remaining changes

--- ProgramCallGraph.py
+++ ProgramCallGraph.py
@@ -2,12 +2,24 @@ from pycallgraph2 import PyCallGraph
 from pycallgraph2.output import GraphvizOutput
 
 
-def testingMethod(n):
+def fact(n):
     if n <= 1:
         return 1
     else:
-        return n * testingMethod(n - 1)
+        return n * fact(n - 1)
+
+
+def Fibonacci(n):
+    if n < 0:
+        print("Incorrect input")
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
 
 
 with PyCallGraph(output=GraphvizOutput()):
-    testingMethod(5)
+    Fibonacci(5)
+    fact(5)
